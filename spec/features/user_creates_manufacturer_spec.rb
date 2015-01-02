77efd7888b5e18fb10d75df4dd9b140ac0b84eeb
graffam <graffam87@gmail.com>
2015-01-02 09:40:55 -0500

    added test for successfully making a new manufacturer

---
+++ spec/features/user_creates_manufacturer_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+# As a car salesperson
+# I want to record a car manufacturer
+# So that I can keep track of the types of cars found in the lot
+#
+# Acceptance Criteria:
+#
+# [] I must specify a manufacturer name and country.
+# [] If I do not specify the required information, I am presented with errors.
+# [] If I specify the required information,
+#    the manufacturer is recorded and I am redirected to the index of manufacturers
+
+feature "User adds a manufactuerer" do
+  scenario "User successfully adds a manufacturer" do
+    visit manufacturer_new_path
+
+    fill_in "name", with: "Ford"
+    fill_in "country", with: "USA"
+
+    click_on "Submit"
+
+    expect(page).to have_content("Ford")
+  end
+
+
+
+end
