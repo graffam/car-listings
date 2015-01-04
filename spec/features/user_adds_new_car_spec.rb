bc214d6dc70af15fc7939e86eafd3dd99aa44102
graffam <graffam87@gmail.com>
2015-01-03 22:25:52 -0500

    Fixed last stylistic error

--- spec/features/user_adds_new_car_spec.rb
+++ spec/features/user_adds_new_car_spec.rb
@@ -10,7 +10,8 @@ require "rails_helper"
 # [X] If I do not specify all of the required information
 #     in the required formats, the car is not recorded and
 #     I am presented with errors.
-# [X] Upon successfully creating a car, I am redirected back to the index of cars.
+# [X] Upon successfully creating a car,
+#     I am redirected back to the index of cars.
 
 context "there is a manufacturer in the database" do
 
