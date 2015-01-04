3c434908d4ccacc8ef9f9506b2f7409e2a0dc5e6
graffam <graffam87@gmail.com>
2015-01-03 21:53:00 -0500

    added test to ensure all fields for car must be filled out

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,9 +4,29 @@ class CarsController < ApplicationController
     @car = Car.new
   end
 
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      flash[:notice] = "Successfully added a Car"
+      redirect_to cars_path
+    else
+      render "new"
+    end
+  end
+
   def index
 
 
   end
 
+  def car_params
+    params.require(:car).permit(
+      :manufacturer_id,
+      :name,
+      :year,
+      :mileage,
+      :color,
+      :description
+      )
+  end
 end
--- spec/features/user_adds_new_car_spec.rb
+++ spec/features/user_adds_new_car_spec.rb
@@ -4,12 +4,12 @@ require "rails_helper"
 #
 # [] I must specify the manufacturer, color, year, and mileage of the car
 # [] Only years from 1920 and above can be specified.
-# [] I can optionally specify a description of the car.
-# [] If I enter all of the required information in the required formats,
+# [X] I can optionally specify a description of the car.
+# [X] If I enter all of the required information in the required formats,
 #    the car is recorded and I am presented with a notification of success.
 # [] If I do not specify all of the required information in the required formats,
 #    the car is not recorded and I am presented with errors.
-# [] Upon successfully creating a car, I am redirected back to the index of cars.
+# [X] Upon successfully creating a car, I am redirected back to the index of cars.
 
 context "there is a manufacturer in the database" do
 
@@ -30,6 +30,18 @@ context "there is a manufacturer in the database" do
     click_on "Submit"
 
     expect(page).to have_content("Successfully added a Car")
+  end
+
+  scenario "User does not fill in req'd fields" do
+
+    visit new_car_path
+
+    click_on "Submit"
+
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Year can't be blank")
+    expect(page).to have_content("Color can't be blank")
+    expect(page).to have_content("Mileage can't be blank")
   end
 
 end
