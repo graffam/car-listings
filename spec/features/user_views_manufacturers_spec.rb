ef1c8948fc63df3e03c9e920923947d5cfaedbcd
graffam <graffam87@gmail.com>
2015-01-04 12:01:12 -0500

    added test for viewing details fo a manufacturer, passes

--- spec/features/user_views_manufacturers_spec.rb
+++ spec/features/user_views_manufacturers_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 # Acceptance Criteria:
-# [] I must see a list of all the manufacturers when I visit manufacturers
-# [] When I visit the details of a manufacturer, I see the details
+# [X] I must see a list of all the manufacturers when I visit manufacturers
+# [X] When I visit the details of a manufacturer, I see the details
 
 context "There is a manufacturer in the DB" do
   before do
@@ -13,5 +13,14 @@ context "There is a manufacturer in the DB" do
     visit manufacturers_path
 
     expect(page).to have_content(@manufacturer.name)
+  end
+
+  scenario "User visits the details of the manufacturer" do
+    visit manufacturers_path
+
+    click_on @manufacturer.name
+
+    expect(page).to have_content(@manufacturer.name)
+    expect(page).to have_content(@manufacturer.country)
   end
 end
